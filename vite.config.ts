e20a16e395efcd2c962a3442515571d5271435fd
sgao <sgao@deloitte.com.cn>
2026-02-09 14:56:54 +0800

    还原对webpack进行的本地修改

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,6 @@ import preact from '@preact/preset-vite';
 import path from 'path';
 import pkg from './package.json';
 
-// WebSocket 拦截器代码（会被内联到脚本头部，在 document-start 时执行）
-const WS_INTERCEPTOR_CODE = `
-(function() {
-  'use strict';
-
-  const sockets = [];
-  const Original = window.WebSocket;
-
-  window.WebSocket = function(url, protocols) {
-    const ws = new Original(url, protocols);
-    sockets.push(ws);
-    window.__capturedSockets = sockets;
-    window.__latestSocket = ws;
-    console.log('[WS拦截器] 捕获:', url);
-    return ws;
-  };
-
-  window.WebSocket.prototype = Original.prototype;
-  window.WebSocket.CONNECTING = Original.CONNECTING;
-  window.WebSocket.OPEN = Original.OPEN;
-  window.WebSocket.CLOSING = Original.CLOSING;
-  window.WebSocket.CLOSED = Original.CLOSED;
-  window.__OriginalWebSocket = Original;
-
-  console.log('[WS拦截器] 已就绪');
-})();
-`;
-
 export default defineConfig({
   server: {
     open: true,
@@ -56,14 +28,6 @@ export default defineConfig({
         updateURL: 'https://github.com/NanGuaChui/moyu-helper/releases/latest/download/moyu-helper.user.js',
         downloadURL: 'https://github.com/NanGuaChui/moyu-helper/releases/latest/download/moyu-helper.user.js',
         grant: ['unsafeWindow', 'GM.getValue', 'GM.setValue', 'GM_addStyle'],
-      },
-      // 在开发模式下，将 WS 拦截器代码注入到 userscript 注释之后
-      generate: ({ userscript, mode }) => {
-        if (mode === 'serve') {
-          // 开发模式：在 userscript 注释后注入拦截器代码
-          return `${userscript}\n${WS_INTERCEPTOR_CODE}\n`;
-        }
-        return userscript;
       },
       server: {
         mountGmApi: true,
